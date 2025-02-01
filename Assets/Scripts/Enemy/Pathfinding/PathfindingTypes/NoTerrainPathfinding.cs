ccbd78dd6e7c14f87085e3cbe7932d9f535d12e2
Sauliusm1 <113345452+Sauliusm1@users.noreply.github.com>
2025-02-01 13:24:18 +0200

    Update NoTerrainPathfinding.cs

--- Assets/Scripts/Enemy/Pathfinding/PathfindingTypes/NoTerrainPathfinding.cs
+++ Assets/Scripts/Enemy/Pathfinding/PathfindingTypes/NoTerrainPathfinding.cs
@@ -7,7 +7,6 @@ public class NoTerrainPathfinding : EnemyPathFinding
     protected override float CalculateWeight(Node node)
     {
         Debug.Log(node.GetDamageMultCoef());
-        //return 1f + Mathf.Pow((node.GetDamageMultCoef()), 2);//Adding one seems to have the best compromise between saftey and speed
-        return Mathf.Pow((node.GetDamageMultCoef()), 2);//Adding one seems to have the best compromise between saftey and speed
+        return 1f + Mathf.Pow((node.GetDamageMultCoef()), 2);//Adding one increases how much the enemy prioritizes saftey over speed
     }
 }
