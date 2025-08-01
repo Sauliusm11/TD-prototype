e5e1b7c861e01a2dfd6f4503bf511b792211954a
Sauliusm11 <88769620+Sauliusm11@users.noreply.github.com>
2025-08-01 11:32:21 +0300

    Test for high projectile speed bug

--- Assets/Tests/Play/ShootingTests.cs
+++ Assets/Tests/Play/ShootingTests.cs
@@ -89,4 +89,48 @@ public class ShootingTest
         yield return new WaitWhile(() => moneyHandler.HasEnoughMoney(1) == false && livesHandler.HasEnoughLives(20));
         Assert.IsTrue(moneyHandler.HasEnoughMoney(1));
     }
+
+    [UnityTest]
+    public IEnumerator FastProjectileShootingPasses([ValueSource(nameof(TestCasesX))] float[] towerPositionsX, [ValueSource(nameof(TestCasesY))] float[] towerPositionsY)
+    {
+        //Wait for the scene to load
+        yield return new WaitWhile(() => sceneLoaded == false);
+        SetupReferences();
+        //Arrange
+        //Get needed managers
+        JsonParser parser = GameObject.Find("JsonParser").GetComponent<JsonParser>();
+        PathfindingManager pathfindingManager = GameObject.Find("PathFindingManager").GetComponent<PathfindingManager>();
+        TowerPlacement towerPlacement = GameObject.Find("Grid").GetComponent<TowerPlacement>();
+        GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Money moneyHandler = GameObject.Find("MoneyHandler").GetComponent<Money>();
+        Lives livesHandler = GameObject.Find("LivesHandler").GetComponent<Lives>();
+        //Load selected level
+        gameManager.ConfirmLoading("Default");
+
+        //Possible that other test cases might need more money
+        gameManager.SetSelectedTower(TowerContainer.getInstance().towers[1]);
+        //Prepare mock to bypass pointer over ui check
+        PointerEventData eventDataMock = new PointerEventData(GameObject.Find("EventSystem").GetComponent<EventSystem>());
+        eventDataMock.position = new Vector3(-1, -1, -1);
+        //Place towers based on given test case
+        for (int i = 0; i < towerPositionsX.Length; i++)
+        {
+            moneyHandler.ResetMoney();
+            Vector3 position = new Vector3(towerPositionsX[i], towerPositionsY[i]);
+            towerPlacement.HandlePlaceTower(position, eventDataMock);
+            //Skip a frame to allow start to fire first
+            yield return null;
+            towerPlacement.ConfirmPlacement();
+        }
+
+        //Act
+        pathfindingManager.CallWave();
+        while (moneyHandler.HasEnoughMoney(1))
+        {
+            moneyHandler.RemoveMoney(1);
+        }
+        yield return new WaitWhile(() => gameManager.IsWaveActive() == false);
+        yield return new WaitWhile(() => moneyHandler.HasEnoughMoney(1) == false && livesHandler.HasEnoughLives(20));
+        Assert.IsTrue(GameObject.FindGameObjectsWithTag("Bullet").Length < 6);
+    }
 }
