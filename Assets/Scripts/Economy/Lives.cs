8a72281ec8698d1a23f2ff3f8336683b7163d8c2
Sauliusm11 <88769620+Sauliusm11@users.noreply.github.com>
2026-02-14 11:14:08 +0200

    Fix Lives bug

--- Assets/Scripts/Economy/Lives.cs
+++ Assets/Scripts/Economy/Lives.cs
@@ -33,7 +33,7 @@ public class Lives : MonoBehaviour
     /// <returns>True if the player has enough lives, false if not</returns>
     public bool HasEnoughLives(int cost)
     {
-        return lives >= cost;
+        return lives > cost;
     }
     /// <summary>
     /// Called by an enemy that has reached the castle
